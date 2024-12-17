3bf92f558fbbc9abdde7eb8fa17f9491ae688f68
lukabajic <lukabajic.dev@gmail.com>
2024-12-17 16:04:35 +0100

    Switch two colors on dark theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,18 +1,18 @@
 export const Colors = {
   light: {
-    primary: 'rgb(42, 157, 143)',
-    background: 'rgb(233, 236, 239)',
-    card: 'rgb(248, 249, 250)',
-    text: 'rgb(33, 37, 41)',
-    border: 'rgb(173, 181, 189)',
-    notification: 'rgb(231, 111, 81)'
+    primary: "rgb(42, 157, 143)",
+    background: "rgb(233, 236, 239)",
+    card: "rgb(248, 249, 250)",
+    text: "rgb(33, 37, 41)",
+    border: "rgb(173, 181, 189)",
+    notification: "rgb(231, 111, 81)",
   },
   dark: {
-    primary: 'rgb(42, 157, 143)',
-    background: 'rgb(52, 58, 64)',
-    card: 'rgb(33, 37, 41)',
-    text: 'rgb(248, 249, 250)',
-    border: 'rgb(173, 181, 189)',
-    notification: 'rgb(231, 111, 81)'
+    primary: "rgb(42, 157, 143)",
+    background: "rgb(33, 37, 41)",
+    card: "rgb(52, 58, 64)",
+    text: "rgb(248, 249, 250)",
+    border: "rgb(173, 181, 189)",
+    notification: "rgb(231, 111, 81)",
   },
 };
