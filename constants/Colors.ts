5ae0e077d86e1ced94183582ba9eed7947dd49f8
lukabajic <lukabajic.dev@gmail.com>
2024-12-17 21:41:16 +0100

    Change colors to hex codes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,18 +1,18 @@
 export const Colors = {
   light: {
-    primary: "rgb(42, 157, 143)",
-    background: "rgb(233, 236, 239)",
-    card: "rgb(248, 249, 250)",
-    text: "rgb(33, 37, 41)",
-    border: "rgb(173, 181, 189)",
+    primary: "#2a9d8f",
+    background: "#e9ecef",
+    card: "#f8f9fa",
+    text: "#212529",
+    border: "#adb5bd",
     notification: "#e76f51",
   },
   dark: {
-    primary: "rgb(42, 157, 143)",
-    background: "rgb(33, 37, 41)",
-    card: "rgb(52, 58, 64)",
-    text: "rgb(248, 249, 250)",
-    border: "rgb(173, 181, 189)",
+    primary: "#2a9d8f",
+    background: "#212529",
+    card: "#343a40",
+    text: "#f8f9fa",
+    border: "#adb5bd",
     notification: "#e76f51",
   },
 };
