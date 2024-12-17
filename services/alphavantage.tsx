d6841e0dcf44a7f505c642b9ab9d49baf34f6425
lukabajic <lukabajic.dev@gmail.com>
2024-12-17 20:50:01 +0100

    Add new route and types

--- services/alphavantage.tsx
+++ services/alphavantage.tsx
@@ -1,14 +1,16 @@
-import { MarketData } from "@/types/marketData";
+import { MarketData, StockDetails } from "@/types/marketData";
 import { ErrorMessages } from "@/utils/constants";
 
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
-interface FetchTopGainersLosersResponse {
-  data: MarketData | null;
+interface FetchResponse<T> {
+  data: T | null;
   error: ErrorMessages | null;
 }
 
-export async function fetchTopGainersLosers(): Promise<FetchTopGainersLosersResponse> {
+export async function fetchTopGainersLosers(): Promise<
+  FetchResponse<MarketData>
+> {
   try {
     const response = await fetch(`${apiUrl}/top-gainers-losers`);
 
@@ -17,6 +19,25 @@ export async function fetchTopGainersLosers(): Promise<FetchTopGainersLosersResp
     }
 
     const data: MarketData = await response.json();
+
+    return { data, error: null };
+  } catch (e) {
+    console.log(e);
+    return { data: null, error: ErrorMessages.NETWORK_ERROR };
+  }
+}
+
+export async function fetchStockDetails(
+  ticker: string,
+): Promise<FetchResponse<StockDetails>> {
+  try {
+    const response = await fetch(`${apiUrl}/company/${ticker}`);
+
+    if (!response.ok) {
+      return { data: null, error: ErrorMessages.RESPONSE_ERROR };
+    }
+
+    const data: StockDetails = await response.json();
 
     return { data, error: null };
   } catch (e) {
--- types/marketData.ts
+++ types/marketData.ts
@@ -13,3 +13,38 @@ export interface MarketData {
   top_losers: Ticker[];
   most_actively_traded: Ticker[];
 }
+
+// Represents Daily Time Series Data
+export interface DailyData {
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+}
+
+export interface MetaData {
+  symbol: string;
+  lastRefreshed: string;
+  timeZone: string;
+}
+
+export interface CompanyOverview {
+  name: string;
+  description: string;
+  sector: string;
+  industry: string;
+  officialSite: string;
+  marketCapitalization: string;
+  peRatio: string;
+  eps: string;
+  dividendYield: string;
+  fiftyTwoWeekHigh: string;
+  fiftyTwoWeekLow: string;
+}
+
+export interface StockDetails {
+  metaData: MetaData;
+  timeSeriesDaily: Record<string, DailyData>;
+  companyOverview: CompanyOverview;
+}
