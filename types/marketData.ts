d18bd38ec26ad782930a4b2435b630b9c410d9bc
lukabajic <lukabajic.dev@gmail.com>
2024-12-17 21:19:46 +0100

    Add new types

--- types/marketData.ts
+++ types/marketData.ts
@@ -14,7 +14,6 @@ export interface MarketData {
   most_actively_traded: Ticker[];
 }
 
-// Represents Daily Time Series Data
 export interface DailyData {
   open: string;
   high: string;
@@ -29,22 +28,18 @@ export interface MetaData {
   timeZone: string;
 }
 
-export interface CompanyOverview {
-  name: string;
-  description: string;
-  sector: string;
-  industry: string;
-  officialSite: string;
-  marketCapitalization: string;
-  peRatio: string;
-  eps: string;
-  dividendYield: string;
-  fiftyTwoWeekHigh: string;
-  fiftyTwoWeekLow: string;
-}
-
 export interface StockDetails {
-  metaData: MetaData;
-  timeSeriesDaily: Record<string, DailyData>;
-  companyOverview: CompanyOverview;
+  MetaData: MetaData;
+  "Time Series (Daily)": Record<string, DailyData>;
+  Name: string;
+  Description: string;
+  Sector: string;
+  Industry: string;
+  OfficialSite: string;
+  MarketCapitalization: string;
+  PeRatio: string;
+  Eps: string;
+  DividendYield: string;
+  "52WeekHigh": string;
+  "52WeekLow": string;
 }
