3657443d56e616c51859e9f4cea70d4fef55e3c6
lukabajic <lukabajic.dev@gmail.com>
2024-12-18 13:02:26 +0100

    Fix types

--- types/marketData.ts
+++ types/marketData.ts
@@ -1,3 +1,5 @@
+import { ExternalPathString } from 'expo-router';
+
 export interface Ticker {
   ticker: string;
   price: string;
@@ -17,14 +19,14 @@ export interface MarketData {
 // API endpoint returns an object with keys like this
 // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
 export enum Keys {
-  MetaData = "Meta Data",
-  LastRefreshed = "3. Last Refreshed",
-  TimeSeries = "Time Series (Daily)",
-  Open = "1. open",
-  High = "2. high",
-  Low = "3. low",
-  Close = "4. close",
-  Volume = "5. volume",
+  MetaData = 'Meta Data',
+  LastRefreshed = '3. Last Refreshed',
+  TimeSeries = 'Time Series (Daily)',
+  Open = '1. open',
+  High = '2. high',
+  Low = '3. low',
+  Close = '4. close',
+  Volume = '5. volume',
 }
 
 export interface DailyData {
@@ -47,11 +49,11 @@ export interface StockDetails {
   Description: string;
   Sector: string;
   Industry: string;
-  OfficialSite: string;
+  OfficialSite?: ExternalPathString | 'None';
   MarketCapitalization: string;
   PeRatio: string;
   Eps: string;
   DividendYield: string;
-  "52WeekHigh": string;
-  "52WeekLow": string;
+  '52WeekHigh': string;
+  '52WeekLow': string;
 }
